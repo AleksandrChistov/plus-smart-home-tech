e132b2fce5a75db15326634e1fafae49466b1507
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-10-30 09:56:22 +0700

    commit offsets in batches

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/service/AggregationStarter.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/service/AggregationStarter.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -17,7 +19,9 @@ import ru.yandex.practicum.telemetry.aggregator.config.KafkaConfig;
 import ru.yandex.practicum.telemetry.aggregator.config.KafkaConfiguration;
 
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
@@ -30,6 +34,12 @@ public class AggregationStarter {
     private final KafkaConfiguration.KafkaSettings kafkaSettings;
 
     private final SnapshotService snapshotService;
+
+    private final Map<TopicPartition, OffsetAndMetadata> currentOffsets = new HashMap<>();
+
+    private final Duration POLL_TIMEOUT = Duration.ofSeconds(1);
+
+    private final int BATCH_SIZE = 200;
 
     /**
      * Метод для начала процесса агрегации данных.
@@ -49,36 +59,17 @@ public class AggregationStarter {
 
             consumer.subscribe(List.of(kafkaSettings.sensorTopic()));
 
-            while(true) {
-                ConsumerRecords<String, SensorEventAvro> records = consumer.poll(Duration.ofSeconds(1));
+            while (true) {
+                ConsumerRecords<String, SensorEventAvro> records = consumer.poll(POLL_TIMEOUT);
+                int count = 0;
 
                 for (ConsumerRecord<String, SensorEventAvro> record : records) {
-                    SensorEventAvro event = record.value();
-
-                    log.info("Получено событие от датчика: {}", event.toString());
-
-                    Optional<SensorsSnapshotAvro> snapshotOpt = snapshotService.updateState(event);
-
-                    snapshotOpt.ifPresent(snapshot -> {
-                        ProducerRecord<String, SensorsSnapshotAvro> message = new ProducerRecord<>(
-                                kafkaSettings.snapshotsTopic(),
-                                event.getHubId(),
-                                snapshot
-                        );
-
-                        log.debug("Отправляем снапшот датчиков в кафку: {}", snapshot);
-
-                        producer.send(message);
-                    });
-
-                    // at-least-once семантика
-                    log.debug("Асинхронно фиксируем смещения обработанных сообщений");
-                    consumer.commitAsync((offsets, exception) -> {
-                        if(exception != null) {
-                            log.warn("Во время фиксации произошла ошибка. Cмещения: {}", offsets, exception);
-                        }
-                    });
+                    handleRecord(record, producer);
+                    manageOffsets(consumer, record, count);
+                    count++;
                 }
+                // фиксируем максимальный оффсет обработанных записей
+                consumer.commitAsync();
             }
         } catch (WakeupException ignored) {
             log.error("Получен WakeupException");
@@ -94,4 +85,55 @@ public class AggregationStarter {
             }
         }
     }
+
+    private void handleRecord(ConsumerRecord<String, SensorEventAvro> record, Producer<String, SensorsSnapshotAvro> producer) {
+        SensorEventAvro event = record.value();
+        log.info("Получено событие от датчика: {}", event.toString());
+
+        Optional<SensorsSnapshotAvro> snapshotOpt = snapshotService.updateState(event);
+
+        if (snapshotOpt.isPresent()) {
+            ProducerRecord<String, SensorsSnapshotAvro> message = new ProducerRecord<>(
+                    kafkaSettings.snapshotsTopic(),
+                    event.getHubId(),
+                    snapshotOpt.get()
+            );
+
+            log.debug("Отправляем снапшот датчиков в кафку: {}", snapshotOpt.get());
+
+            producer.send(message, (metadata, exception) -> {
+                if (exception != null) {
+                    log.error("Ошибка при отправке сообщения в Kafka, topic: {}, key: {}",
+                            kafkaSettings.snapshotsTopic(), event.getHubId(), exception);
+                } else {
+                    log.info("Сообщение успешно отправлено в Kafka, topic: {}, partition: {}, offset: {}",
+                            metadata.topic(), metadata.partition(), metadata.offset());
+                }
+            });
+        }
+    }
+
+    private void manageOffsets(
+            Consumer<String, SensorEventAvro> consumer,
+            ConsumerRecord<String, SensorEventAvro> record,
+            int count
+    ) {
+        currentOffsets.put(
+                new TopicPartition(record.topic(), record.partition()),
+                new OffsetAndMetadata(record.offset() + 1)
+        );
+
+        // at-least-once семантика
+        if (count % BATCH_SIZE == 0) {
+            log.debug("Асинхронно фиксируем смещения обработанных сообщений");
+            consumer.commitAsync(currentOffsets, (offsets, exception) -> {
+                if (exception != null) {
+                    log.warn("Ошибка при фиксации смещений: {}", offsets, exception);
+                } else {
+                    log.debug("Успешно зафиксированы смещения: {}", offsets);
+                }
+            });
+        }
+    }
+
 }
