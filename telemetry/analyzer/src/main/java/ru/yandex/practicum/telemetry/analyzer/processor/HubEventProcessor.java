35636f314f5ebfd51fd333640259f8eebfb2e1be
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-10-30 12:59:33 +0700

    replace AnalyzerRunner with @EventListener and @Async

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/processor/HubEventProcessor.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/processor/HubEventProcessor.java
@@ -8,6 +8,9 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.kafka.telemetry.event.*;
 import ru.yandex.practicum.telemetry.analyzer.kafka.KafkaFactory;
@@ -21,7 +24,7 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class HubEventProcessor implements Runnable {
+public class HubEventProcessor {
 
     private final KafkaFactory kafkaFactory;
 
@@ -31,50 +34,55 @@ public class HubEventProcessor implements Runnable {
 
     private final Map<TopicPartition, OffsetAndMetadata> currentOffsets = new HashMap<>();
 
-    @Override
+    @Async
+    @EventListener(ApplicationReadyEvent.class)
     public void run() {
-        final Consumer<String, HubEventAvro> consumer = kafkaFactory.createConsumer(HubEventProcessor.class.getSimpleName());
-        final Duration pollTimeout = Duration.ofMillis(kafkaFactory.getPollTimeout(HubEventProcessor.class.getSimpleName()));
+       try {
+           final Consumer<String, HubEventAvro> consumer = kafkaFactory.createConsumer(HubEventProcessor.class.getSimpleName());
+           final Duration pollTimeout = Duration.ofMillis(kafkaFactory.getPollTimeout(HubEventProcessor.class.getSimpleName()));
 
-        try {
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                log.info("Получен сигнал на завершение работы в {}", HubEventProcessor.class.getSimpleName());
-                consumer.wakeup();
-            }));
+           try {
+               Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                   log.info("Получен сигнал на завершение работы в {}", HubEventProcessor.class.getSimpleName());
+                   consumer.wakeup();
+               }));
 
-            consumer.subscribe(kafkaFactory.getTopics(HubEventProcessor.class.getSimpleName()));
+               consumer.subscribe(kafkaFactory.getTopics(HubEventProcessor.class.getSimpleName()));
 
-            while (true) {
-                ConsumerRecords<String, HubEventAvro> records = consumer.poll(pollTimeout);
+               while (true) {
+                   ConsumerRecords<String, HubEventAvro> records = consumer.poll(pollTimeout);
 
-                for (ConsumerRecord<String, HubEventAvro> record : records) {
-                    handleRecord(record);
-                    // at-least-once семантика
-                    currentOffsets.put(
-                            new TopicPartition(record.topic(), record.partition()),
-                            new OffsetAndMetadata(record.offset() + 1)
-                    );
-                    consumer.commitAsync(currentOffsets, (offsets, exception) -> {
-                        if (exception != null) {
-                            log.warn("Во время фиксации произошла ошибка. Cмещения: {}", offsets, exception);
-                        } else {
-                            log.debug("Успешно зафиксированы смещения: {}", offsets);
-                        }
-                    });
-                }
-            }
-        } catch (WakeupException ignored) {
-            log.error("Получен WakeupException");
-        } catch (Exception e) {
-            log.error("Ошибка во время обработки событий датчиков", e);
-        } finally {
-            try {
-                log.debug("Фиксация смещений");
-                consumer.commitSync();
-            } finally {
-                consumer.close();
-            }
-        }
+                   for (ConsumerRecord<String, HubEventAvro> record : records) {
+                       handleRecord(record);
+                       // at-least-once семантика
+                       currentOffsets.put(
+                               new TopicPartition(record.topic(), record.partition()),
+                               new OffsetAndMetadata(record.offset() + 1)
+                       );
+                       consumer.commitAsync(currentOffsets, (offsets, exception) -> {
+                           if (exception != null) {
+                               log.warn("Во время фиксации произошла ошибка. Cмещения: {}", offsets, exception);
+                           } else {
+                               log.debug("Успешно зафиксированы смещения: {}", offsets);
+                           }
+                       });
+                   }
+               }
+           } catch (WakeupException ignored) {
+               log.error("Получен WakeupException");
+           } catch (Exception e) {
+               log.error("Ошибка во время обработки событий датчиков", e);
+           } finally {
+               try {
+                   log.debug("Фиксация смещений");
+                   consumer.commitSync();
+               } finally {
+                   consumer.close();
+               }
+           }
+       } catch (Exception e) {
+           log.error("Ошибка создания консьюмера в {}", HubEventProcessor.class.getSimpleName(), e);
+       }
     }
 
     private void handleRecord(ConsumerRecord<String, HubEventAvro> record) {
