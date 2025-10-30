61df14f4066395f954825e628216861094a060e6
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-10-30 12:59:33 +0700

    replace AnalyzerRunner with @EventListener and @Async

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/Analyzer.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/Analyzer.java
@@ -3,8 +3,10 @@ package ru.yandex.practicum.telemetry.analyzer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 @ConfigurationPropertiesScan
 public class Analyzer {
     public static void main(String[] args) {
