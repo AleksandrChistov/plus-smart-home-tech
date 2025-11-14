6666403ccccd72b829cc8f51a8cb1af57711299c
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-11-14 11:02:00 +0700

    remove redundant @Repository annotation

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/shoppingstore/dal/dao/ProductRepository.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/shoppingstore/dal/dao/ProductRepository.java
@@ -2,13 +2,11 @@ package ru.yandex.practicum.shoppingstore.dal.dao;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.api.shopping.store.enums.ProductCategory;
 import ru.yandex.practicum.shoppingstore.dal.model.Product;
 
 import java.util.List;
 
-@Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
 
     List<Product> findByProductCategory(ProductCategory category, Pageable pageable);
--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/dal/dao/WarehouseProductRepository.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/dal/dao/WarehouseProductRepository.java
@@ -3,13 +3,11 @@ package ru.yandex.practicum.warehouse.dal.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.warehouse.dal.model.WarehouseProduct;
 
 import java.util.Collection;
 import java.util.List;
 
-@Repository
 public interface WarehouseProductRepository extends JpaRepository<WarehouseProduct, String> {
 
     boolean existsByProductId(String productId);
