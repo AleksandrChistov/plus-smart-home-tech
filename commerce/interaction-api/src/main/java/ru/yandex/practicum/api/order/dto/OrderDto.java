04b5c0bd5b9d4eea84f6b4a6679e86a46823454e
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-11-18 12:25:02 +0700

    create DTO for order

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/api/order/dto/OrderDto.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.api.order.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import ru.yandex.practicum.api.order.enums.OrderState;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderDto {
+    @NotBlank(message = "ID заказа не может быть пустым")
+    String orderId;
+    String shoppingCartId;
+    @NotNull(message = "Список товаров не может отсутствовать")
+    @Builder.Default
+    Map<String, Integer> products = new HashMap<>();
+    String paymentId;
+    String deliveryId;
+    @NotNull(message = "Состояние заказа должно быть указано")
+    @Builder.Default
+    OrderState state = OrderState.NEW;
+    Float deliveryWeight;
+    Float deliveryVolume;
+    Boolean fragile;
+    BigDecimal totalPrice;
+    BigDecimal deliveryPrice;
+    BigDecimal productPrice;
+}
