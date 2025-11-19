9147dd68025bc2342c683fd1c36d8bfaf31968db
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-11-19 12:31:48 +0700

    add entity to order

---
+++ commerce/order/src/main/java/ru/yandex/practicum/order/dal/model/Order.java
@@ -0,0 +1,68 @@
+package ru.yandex.practicum.order.dal.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+import ru.yandex.practicum.api.order.enums.OrderState;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @Column(name = "order_id")
+    @UuidGenerator
+    private String orderId;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "shopping_cart_id")
+    private String shoppingCartId;
+
+    @Column(name = "payment_id")
+    private String paymentId;
+
+    @Column(name = "delivery_id")
+    private String deliveryId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state", nullable = false)
+    private OrderState state;
+
+    @Column(name = "delivery_weight")
+    private Float deliveryWeight;
+
+    @Column(name = "delivery_volume")
+    private Float deliveryVolume;
+
+    @Column(name = "fragile")
+    private Boolean fragile;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @Column(name = "delivery_price")
+    private BigDecimal deliveryPrice;
+
+    @Column(name = "product_price")
+    private BigDecimal productPrice;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "order_products",
+            joinColumns = @JoinColumn(name = "order_id"),
+            uniqueConstraints = @UniqueConstraint(columnNames = {"order_id", "product_id"})
+    )
+    @MapKeyColumn(name = "product_id")
+    @Column(name = "quantity")
+    private Map<String, Integer> products = new HashMap<>();
+}
