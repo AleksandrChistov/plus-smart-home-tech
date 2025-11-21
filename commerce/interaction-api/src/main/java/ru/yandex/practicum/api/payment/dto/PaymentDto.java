7e7fb761d721a60753e963960dc36f412e1c1d00
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-11-21 12:05:28 +0700

    create entity for payment

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/api/payment/dto/PaymentDto.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/api/payment/dto/PaymentDto.java
@@ -19,14 +19,14 @@ public class PaymentDto {
     String paymentId;
     @NotNull(message = "Общая стоимость должна быть указана")
     @DecimalMin(value = "0.00", message = "Общая стоимость не может быть меньше 0 руб.")
-    @Digits(integer = 17, fraction = 2, message = "Общая стоимость поддерживает максимум 17 цифр и 2 после запятой")
+    @Digits(integer = 19, fraction = 2, message = "Общая стоимость поддерживает максимум 19 цифр и 2 после запятой")
     BigDecimal totalPayment;
     @NotNull(message = "Стоимость доставки должна быть указана")
     @DecimalMin(value = "0.00", message = "Стоимость доставки не может быть меньше 0 руб.")
-    @Digits(integer = 17, fraction = 2, message = "Стоимость доставки поддерживает максимум 17 цифр и 2 после запятой")
+    @Digits(integer = 19, fraction = 2, message = "Стоимость доставки поддерживает максимум 19 цифр и 2 после запятой")
     BigDecimal deliveryTotal;
     @NotNull(message = "Стоимость налога должна быть указана")
     @DecimalMin(value = "0.00", message = "Стоимость налога не может быть меньше 0 руб.")
-    @Digits(integer = 17, fraction = 2, message = "Стоимость налога поддерживает максимум 17 цифр и 2 после запятой")
+    @Digits(integer = 19, fraction = 2, message = "Стоимость налога поддерживает максимум 19 цифр и 2 после запятой")
     BigDecimal feeTotal;
 }
