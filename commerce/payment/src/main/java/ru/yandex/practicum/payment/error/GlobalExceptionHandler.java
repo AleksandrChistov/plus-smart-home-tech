e385d9508d03021ee13a27adad4c5dce6f11e7fc
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-11-22 12:02:00 +0700

    create GlobalExceptionHandler

---
+++ commerce/payment/src/main/java/ru/yandex/practicum/payment/error/GlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package ru.yandex.practicum.payment.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import ru.yandex.practicum.api.shared.error.ApiError;
+import ru.yandex.practicum.api.shared.error.NotFoundException;
+import ru.yandex.practicum.api.shared.error.ServiceUnavailableException;
+
+import java.util.Optional;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiError> handleDataIntegrityViolationException(final DataIntegrityViolationException ex) {
+        log.warn("409 Conflict: {}", ex.getMessage());
+        return getResponseEntity(HttpStatus.CONFLICT, ex.getMessage(), null);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleInvalidInput(MethodArgumentNotValidException ex) {
+        log.warn("400 {}", ex.getMessage());
+
+        String message = Optional
+                .ofNullable(ex.getBindingResult().getAllErrors().getFirst())
+                .map(ObjectError::getDefaultMessage)
+                .orElse(ex.getMessage());
+
+        return getResponseEntity(HttpStatus.BAD_REQUEST, message, null);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ApiError> handleNotFoundException(final NotFoundException ex) {
+        log.warn("404 {}", ex.getMessage());
+        return getResponseEntity(HttpStatus.NOT_FOUND, ex.getMessage(), null);
+    }
+
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<ApiError> handleMethodValidationException(final HandlerMethodValidationException ex) {
+        log.warn("422 {}", ex.getMessage());
+        return getResponseEntity(HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage(), null);
+    }
+
+    @ExceptionHandler(ServiceUnavailableException.class)
+    public ResponseEntity<ApiError> handleServiceUnavailableException(final ServiceUnavailableException ex) {
+        log.error("503 {}", ex.getMessage(), ex);
+        return getResponseEntity(HttpStatus.SERVICE_UNAVAILABLE, ex.getMessage(), null);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleException(final Exception ex) {
+        log.error("500 {}", ex.getMessage(), ex);
+        return getResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), null);
+    }
+
+    private ResponseEntity<ApiError> getResponseEntity(HttpStatus httpStatus, String message, Object info) {
+        ApiError apiError = new ApiError(httpStatus, message, info);
+        return new ResponseEntity<>(apiError, httpStatus);
+    }
+
+}
