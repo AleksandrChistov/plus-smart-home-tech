aaf7d29fc3d0a4790d436a0d0295ae821991539a
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-11-23 12:02:50 +0700

    add new methods to WarehouseApi

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/api/warehouse/service/WarehouseApi.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/api/warehouse/service/WarehouseApi.java
@@ -5,12 +5,11 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.api.shared.error.NotFoundException;
 import ru.yandex.practicum.api.shopping.cart.dto.ShoppingCartDto;
-import ru.yandex.practicum.api.warehouse.dto.AddProductToWarehouseRequest;
-import ru.yandex.practicum.api.warehouse.dto.AddressDto;
-import ru.yandex.practicum.api.warehouse.dto.BookedProductsDto;
-import ru.yandex.practicum.api.warehouse.dto.NewProductInWarehouseRequest;
+import ru.yandex.practicum.api.warehouse.dto.*;
 import ru.yandex.practicum.api.warehouse.error.InsufficientStockError;
 import ru.yandex.practicum.api.warehouse.error.ProductAlreadyExistError;
+
+import java.util.Map;
 
 public interface WarehouseApi {
     String URL = "/api/v1/warehouse";
@@ -24,8 +23,26 @@ public interface WarehouseApi {
     @PostMapping(path = URL + "/check", consumes = MediaType.APPLICATION_JSON_VALUE)
     BookedProductsDto checkProducts(@RequestBody @Valid ShoppingCartDto shoppingCartDto) throws InsufficientStockError;
 
+    /**
+     * Собрать товары к заказу для подготовки к отправке.
+     */
+    @PostMapping(path = URL + "/assembly", consumes = MediaType.APPLICATION_JSON_VALUE)
+    BookedProductsDto assemblyProducts(@RequestBody @Valid AssemblyProductsForOrderRequest request) throws InsufficientStockError;
+
+    /**
+     * Передать товары в доставку.
+     */
+    @PostMapping(path = URL + "/shipped", consumes = MediaType.APPLICATION_JSON_VALUE)
+    void shipProducts(@RequestBody @Valid ShippedToDeliveryRequest request) throws NotFoundException;
+
     @PostMapping(path = URL + "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
     void addQuantity(@RequestBody @Valid AddProductToWarehouseRequest addProductRequest) throws NotFoundException;
+
+    /**
+     * Принять возврат товаров на склад.
+     */
+    @PostMapping(path = URL + "/return", consumes = MediaType.APPLICATION_JSON_VALUE)
+    void returnProducts(@RequestBody @Valid Map<String, Integer> products);
 
     /**
      * Предоставить адрес склада для расчёта доставки.
