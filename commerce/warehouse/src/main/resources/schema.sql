db6825353916263dc55f20334ebbc1e0b9808397
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-11-23 12:11:03 +0700

    create WarehouseOrder entity

--- commerce/warehouse/src/main/resources/schema.sql
+++ commerce/warehouse/src/main/resources/schema.sql
@@ -4,10 +4,24 @@ CREATE TABLE IF NOT EXISTS warehouse_products (
     width NUMERIC(5,2) NOT NULL CHECK (width >= 1),
     height NUMERIC(5,2) NOT NULL CHECK (height >= 1),
     depth NUMERIC(5,2) NOT NULL CHECK (depth >= 1),
-    weight NUMERIC(4,2) NOT NULL CHECK (weight >= 1),
+    weight NUMERIC(4,2) NOT NULL CHECK (weight >= 1)
 );
 
 CREATE TABLE IF NOT EXISTS warehouse_stock (
     product_id VARCHAR(36) PRIMARY KEY, -- UUID format
     quantity INT NOT NULL DEFAULT 0 CHECK (quantity >= 0),
+    FOREIGN KEY (product_id) REFERENCES warehouse_products(product_id)
+);
+
+CREATE TABLE IF NOT EXISTS warehouse_orders (
+    order_id VARCHAR(36) PRIMARY KEY,
+    delivery_id VARCHAR(36) UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS warehouse_orders_products (
+    order_id VARCHAR(36) NOT NULL,
+    product_id VARCHAR(36) NOT NULL,
+    PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES warehouse_orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES warehouse_products(product_id)
 );
