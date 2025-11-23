d95ed101b4654687a777436c2e8d883fb130d0f1
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-11-23 12:34:06 +0700

    implements new method in WarehouseProductService

--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/dal/dao/WarehouseStockRepository.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/dal/dao/WarehouseStockRepository.java
@@ -3,10 +3,14 @@ package ru.yandex.practicum.warehouse.dal.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.yandex.practicum.warehouse.dal.model.WarehouseStock;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface WarehouseStockRepository extends JpaRepository<WarehouseStock, String> {
 
     Optional<WarehouseStock> findByProductId(String productId);
+
+    List<WarehouseStock> findAllByProductIdIn(Collection<String> productIds);
 
 }
