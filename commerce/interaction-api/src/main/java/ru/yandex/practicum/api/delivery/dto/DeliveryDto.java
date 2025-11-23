030e3ccd85d7497e397dbf24fc1ce24e4ca4c934
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-11-23 17:42:03 +0700

    fix DeliveryDto

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/api/delivery/dto/DeliveryDto.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/api/delivery/dto/DeliveryDto.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.api.delivery.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import ru.yandex.practicum.api.delivery.enums.DeliveryState;
 import ru.yandex.practicum.api.warehouse.dto.AddressDto;
@@ -17,8 +18,8 @@ public class DeliveryDto {
     String orderId;
     @Builder.Default
     DeliveryState deliveryState = DeliveryState.CREATED;
-    @NotBlank(message = "Адрес отправки не может быть пустым")
+    @NotNull(message = "Адрес отправки не может быть пустым")
     AddressDto fromAddress;
-    @NotBlank(message = "Адрес доставки не может быть пустым")
+    @NotNull(message = "Адрес доставки не может быть пустым")
     AddressDto toAddress;
 }
