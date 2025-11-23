40b481f04df73a2296f78e54434f2668973801b5
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-11-23 17:44:20 +0700

    fix WarehouseProductService

--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/dal/dao/WarehouseOrderRepository.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/dal/dao/WarehouseOrderRepository.java
@@ -3,12 +3,10 @@ package ru.yandex.practicum.warehouse.dal.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.yandex.practicum.warehouse.dal.model.WarehouseOrder;
 
-import java.util.Collection;
 import java.util.Optional;
 
 public interface WarehouseOrderRepository extends JpaRepository<WarehouseOrder, String> {
 
     Optional<WarehouseOrder> findByOrderId(String orderId);
 
-    void deleteAllByProductIdIn(Collection<String> productIds);
 }
