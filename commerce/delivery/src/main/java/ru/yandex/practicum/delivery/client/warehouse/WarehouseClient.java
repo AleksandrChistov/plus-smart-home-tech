e4641a9e451e59f5e713cc09e5c05831a1bec974
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-11-23 17:40:56 +0700

    fix WarehouseClient in delivery

--- commerce/delivery/src/main/java/ru/yandex/practicum/delivery/client/warehouse/WarehouseClient.java
+++ commerce/delivery/src/main/java/ru/yandex/practicum/delivery/client/warehouse/WarehouseClient.java
@@ -2,8 +2,8 @@ package ru.yandex.practicum.delivery.client.warehouse;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.yandex.practicum.api.warehouse.service.WarehouseApi;
-import ru.yandex.practicum.delivery.config.ClientConfig;
+import ru.yandex.practicum.delivery.config.WarehouseClientConfig;
 
-@FeignClient(name = "warehouse", configuration = ClientConfig.class, fallback = WarehouseClientFallback.class)
+@FeignClient(name = "warehouse", configuration = WarehouseClientConfig.class, fallback = WarehouseClientFallback.class)
 public interface WarehouseClient extends WarehouseApi {
 }
