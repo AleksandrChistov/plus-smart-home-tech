e443562ba38970efb19ee38a634d90ccc397bf0f
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-11-23 17:42:33 +0700

    fix OrderClient in payment

--- commerce/payment/src/main/java/ru/yandex/practicum/payment/client/order/OrderClient.java
+++ commerce/payment/src/main/java/ru/yandex/practicum/payment/client/order/OrderClient.java
@@ -2,8 +2,8 @@ package ru.yandex.practicum.payment.client.order;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.yandex.practicum.api.order.service.OrderApi;
-import ru.yandex.practicum.payment.config.ClientConfig;
+import ru.yandex.practicum.payment.config.OrderClientConfig;
 
-@FeignClient(name = "order", configuration = ClientConfig.class, fallback = OrderClientFallback.class)
+@FeignClient(name = "order", configuration = OrderClientConfig.class, fallback = OrderClientFallback.class)
 public interface OrderClient extends OrderApi {
 }
---
+++ commerce/payment/src/main/java/ru/yandex/practicum/payment/config/OrderClientConfig.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.payment.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Feign;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.yandex.practicum.payment.client.order.OrderClientErrorDecoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class OrderClientConfig {
+
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public Feign.Builder feignOrderBuilder() {
+        return Feign.builder()
+                .errorDecoder(new OrderClientErrorDecoder(objectMapper));
+    }
+
+}
+
+
