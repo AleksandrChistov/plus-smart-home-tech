e9186ab85d42ac0d3e2f2dfa6514abf18ea4bb5b
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-11-23 17:41:38 +0700

    fix OrderClient in delivery

--- commerce/delivery/src/main/java/ru/yandex/practicum/delivery/config/OrderClientConfig.java
+++ commerce/delivery/src/main/java/ru/yandex/practicum/delivery/config/OrderClientConfig.java
@@ -6,11 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.delivery.client.order.OrderClientErrorDecoder;
-import ru.yandex.practicum.delivery.client.warehouse.WarehouseClientErrorDecoder;
 
 @Configuration
 @RequiredArgsConstructor
-public class ClientConfig {
+public class OrderClientConfig {
 
     private final ObjectMapper objectMapper;
 
@@ -18,12 +17,6 @@ public class ClientConfig {
     public Feign.Builder feignOrderBuilder() {
         return Feign.builder()
                 .errorDecoder(new OrderClientErrorDecoder(objectMapper));
-    }
-
-    @Bean
-    public Feign.Builder feignWarehouseBuilder() {
-        return Feign.builder()
-                .errorDecoder(new WarehouseClientErrorDecoder(objectMapper));
     }
 
 }
